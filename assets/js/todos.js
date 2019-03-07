0d43859e2b842e92452d4e4b1417b44dc84982b4
Andrea Gimenez <gim.andr26@gmail.com>
2019-03-07 17:23:33 -0300

    avance

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -8,3 +8,12 @@ $("span").click(function(event){
   });
   event.stopPropagation();
 });
+
+$("input[type = 'text']").keypress(function(event){
+  if(event.which === 13){ //13 es la tecla enter
+    var text = $(this).val();
+    //erase the input text
+    $(this).val("");
+    $("ul").append("<li>" + text + "</li>");
+  }
+})
